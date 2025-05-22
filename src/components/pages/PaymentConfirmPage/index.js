8fc0b63dcc8004379569ee457c491332a26a01c9
ray <pamungkasray229@gmail.com>
2025-05-22 18:57:47 +0700

    update

--- src/components/pages/PaymentConfirmPage/index.js
+++ src/components/pages/PaymentConfirmPage/index.js
@@ -333,7 +333,6 @@ const PaymentConfirmPage = () => {
     }
   }, []);
 
-
   const currCard = (card, id = null) => {
     const xCard = card.toLowerCase();
     if (xCard === "mandiri") {
@@ -421,12 +420,7 @@ const PaymentConfirmPage = () => {
     //     elementRef.current.style.display = "none";
     //   });
 
-    history.push({
-      pathname: "./show-receipt",
-      state: {
-        data: cartData,
-      },
-    });
+    history.push("/show-receipt/?trans_order_id=" + cartData.id);
   };
 
   return (
